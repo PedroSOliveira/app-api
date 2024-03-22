fc2952a11c8a863eb20c71e3b878a165fa1523a4
Pedro Oliveira <pedro.oliveira@sps.ce.gov.br>
2024-03-22 16:45:37 -0300

    chore: add jest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfigModule } from './shared/infraestructure/env-config/env-config.module';
 
 @Module({
-  imports: [],
+  imports: [EnvConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
